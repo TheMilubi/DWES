377b242cfa9ef63a4d388512b574733b64314449
Ruben Diaz <TheMilubi@gmail.com>
2019-10-04 09:18:45 +0200

    THE END UT1

--- EjercicioUT0104.php
+++ EjercicioUT0104.php
@@ -13,12 +13,16 @@
         ?>
         
             <tr><th>Lunes</th><th>Martes</th><th>Miercoles</th><th>Jueves</th><th>Viernes</th></tr>
-            <tr><td rowspan="3">DWES</td><td rowspan="2">EIE</td><td rowspan="2">DIW</td></tr>
-            <tr><td>vaso</td><td>glass</td></tr>
-            <tr><td>dedo</td><td>finger</td></tr>
-            <tr><td>casa</td><td>house</td></tr>
-            <tr><td>cuerpo</td><td>body</td></tr>
-            <tr><td>pierna</td><td>leg</td></tr>
+            <tr><td>DWES</td><td>EIE</td><td>DIW</td><td>DIW</td><td>DWES</td></tr>
+            <tr><td>DWES</td><td>EIE</td><td>DIW</td><td>DIW</td><td>DWES</td></tr>
+            <?php
+            echo '<tr><td>DWES</td><td>DAW</td><td>DWES</td><td>DIW</td><td>DWEC</td></tr>';
+            echo '<tr><td colspan="5">Recreo</td></tr>';
+            ?>
+            <tr><td>DWEC</td><td>DIW</td><td>DWES</td><td>DAW</td><td>DWEC</td></tr>
+            <tr><td>DWEC</td><td>DIW</td><td>DWES</td><td>DAW</td><td>DWEC</td></tr>
+            <tr><td>DWEC</td><td>DIW</td><td>EIE</td><td>EIE</td><td>DIW</td></tr>
+            
         <?php
             echo '</table>';
         ?>
